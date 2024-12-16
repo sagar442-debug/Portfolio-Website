6bf628e65c1c3565f1de228479846a803efab3b9
Sagar Sapkota <67532085+sagar442-debug@users.noreply.github.com>
2024-12-16 15:46:36 -0700

    Fixing the projects page

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -9,7 +9,7 @@ import Link from "next/link";
 
 const RecentProjects = () => {
   return (
-    <div className="py-20">
+    <div id="#projects" className="py-20">
       <h1 className="heading">
         A small selection of{" "}
         <span className="text-purple">recent projects</span>
