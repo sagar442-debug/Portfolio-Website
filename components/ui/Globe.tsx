e596528165a81e18f183c5359261355cc565d2fb
Sagar Sapkota <67532085+sagar442-debug@users.noreply.github.com>
2024-12-16 03:11:28 -0700

    Fx

--- components/ui/Globe.tsx
+++ components/ui/Globe.tsx
@@ -178,7 +178,7 @@ export function Globe({ globeConfig, data }: WorldProps) {
       .arcStartLng((d) => (d as { startLng: number }).startLng * 1)
       .arcEndLat((d) => (d as { endLat: number }).endLat * 1)
       .arcEndLng((d) => (d as { endLng: number }).endLng * 1)
-      .arcColor((e: { color: string }) => e.color) // Fix for type issue with arcColor
+      .arcColor((e: { color: string }) => e.color)
       .arcAltitude((e) => (e as { arcAlt: number }).arcAlt * 1)
       .arcStroke(() => [0.32, 0.28, 0.3][Math.round(Math.random() * 2)])
       .arcDashLength(defaultProps.arcLength)
@@ -195,7 +195,7 @@ export function Globe({ globeConfig, data }: WorldProps) {
 
     globeRef.current
       .ringsData([])
-      .ringColor((e: { color: string }) => (t: number) => e.color)
+      .ringColor((e: { color: string }) => e.color)
       .ringMaxRadius(defaultProps.maxRings)
       .ringPropagationSpeed(RING_PROPAGATION_SPEED)
       .ringRepeatPeriod(
@@ -271,7 +271,7 @@ export function World(props: WorldProps) {
 }
 
 function genRandomNumbers(min: number, max: number, count: number) {
-  let numbers = [];
+  const numbers = [];
   for (let i = 0; i < count; i++) {
     let num;
     do {
