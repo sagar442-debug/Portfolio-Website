b369634a886de9b4c15a067d6ad32ad276e59116
Sagar Sapkota <67532085+sagar442-debug@users.noreply.github.com>
2024-12-16 03:39:24 -0700

    Fixing again

--- components/ui/Globe.tsx
+++ components/ui/Globe.tsx
@@ -194,7 +194,7 @@ export function Globe({ globeConfig, data }: WorldProps) {
 
     globeRef.current
       .ringsData([])
-      .ringColor((e: any) => (t: any) => e.color(t))
+      // .ringColor((e: any) => (t: any) => e.color(t))
       .ringMaxRadius(defaultProps.maxRings)
       .ringPropagationSpeed(RING_PROPAGATION_SPEED)
       .ringRepeatPeriod(
