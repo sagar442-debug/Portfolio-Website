ae47a4207d437681a1865cfd553f7ceca0447dc2
Sagar Sapkota <67532085+sagar442-debug@users.noreply.github.com>
2024-12-16 03:26:56 -0700

    Fixing again

--- components/ui/CanvasRevealEffect.tsx
+++ components/ui/CanvasRevealEffect.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
-import { cn } from "@/lib/utils/cn";
+import { cn } from "@/lib/utils";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import React, { useMemo, useRef } from "react";
 import * as THREE from "three";
