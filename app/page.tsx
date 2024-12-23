123f346c1c79398d9c813cfd32de6deefea80682
Sagar Sapkota <67532085+sagar442-debug@users.noreply.github.com>
2024-12-23 14:49:41 -0700

    Fixing the relocation of the hero section

--- app/page.tsx
+++ app/page.tsx
@@ -13,8 +13,8 @@ import { FloatingNav } from "@/components/ui/FloatingNav";
 
 const Home = () => {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 scroll-m-40  ">
+      <div className="max-w-7xl w-full mt-56">
         <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,7 +18,7 @@ const Hero = () => {
         />
         <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
       </div>
-      <div className="h-screen w-full absolute top-0 left-0  dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2]  flex items-center justify-center">
+      <div className="h-screen w-full absolute  top-0 left-0  dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2]  flex items-center justify-center">
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
       </div>
       <div className="flex justify-center relative my-20 z-10 ">
